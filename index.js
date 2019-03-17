ce1975c736e86cf21e71db6bf31781335bbfb9d8
Maryasych <maryasych@gmail.com>
2019-03-17 22:42:32 +0200

    mime check

--- index.js
+++ index.js
@@ -6,27 +6,34 @@ var storage = multer.diskStorage({
     destination: './public/uploads/',
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now())
-    }
+    },
 })
 
 var upload = multer({
-    storage: storage
-}).single('file');
+            fileFilter: function (req, file, cb) {
+                let mimes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif', 'application/pdf'];
+                if (mimes.some(mime => mime == file.mimetype)) {
+                        cb(null, true)
+                    } else {
+                        cb(new Error("Недопустиме розширення файлу!"), false)
+                    };
+                },
+                storage: storage
+            }).single('file');
 
-app.use(express.static('./public'));
 
-app.get('/', (res, req) => render('index'))
+        app.use(express.static('./public'));
 
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(req.file);
-            req.send('File sended')
-        }
-    })
-    })
+        app.get('/', (res, req) => render('index'))
 
-app.listen(3000)
-console.log('listening port')
+        app.post('/upload', (req, res) => {
+            upload(req, res, (err) => {
+                if (err) {
+                    res.sendStatus(415).send(err.message)
+                } else {
+                    res.sendStatus(200)
+                }
+            })
+        })
+
+        app.listen(3000); console.log('listening port')
