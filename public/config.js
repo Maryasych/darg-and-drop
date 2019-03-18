c3aaf96dc4098577a8932392e3a03246fc10dfac
Maryasych <maryasych@gmail.com>
2019-03-18 09:49:35 +0200

    fetch data

--- public/config.js
+++ public/config.js
@@ -1,12 +1,12 @@
 let dragForm = document.querySelector('form'),
- gallery = document.querySelector('#gallery');
+  gallery = document.querySelector('#gallery');
 
 ['dragover', 'dragleave', 'drop'].forEach(eventName => {
-  dragForm.addEventListener(eventName, opacityAndPreventDefault)   
+  dragForm.addEventListener(eventName, opacityAndPreventDefault)
   document.addEventListener(eventName, opacityAndPreventDefault)
 });
 
-function opacityAndPreventDefault(e){
+function opacityAndPreventDefault(e) {
   e.type == 'dragover' ? dragForm.style.opacity = .5 : 0
   e.type == 'dragleave' || e.type == 'drop' ? dragForm.style.opacity = 1 : 0
   e.preventDefault();
@@ -15,7 +15,14 @@ function opacityAndPreventDefault(e){
 
 dragForm.addEventListener('drop', sendFile)
 
-function sendFile(e){
-
+function sendFile(e) {
+  let dt = e.dataTransfer;
+  let files = dt.files;
+  let filesFetch = new FormData();
+  filesFetch.append('file', ...files)
+  let options = {
+    method: "POST",
+    body: filesFetch
+  }
+    fetch('/upload', options)
 }
-
